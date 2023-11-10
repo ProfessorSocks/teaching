9e89d3a78fbd16826e9391815c6afefe5e4d6447
ProfessorSocks <124716422+ProfessorSocks@users.noreply.github.com>
2023-11-09 19:02:22 -0800

    Initial commit

---
+++ variablesandoperations.js
@@ -0,0 +1,128 @@
+//This is a comment it is denoted by //
+/*you can create multiple line comments like this
+
+hi.
+everything inside is a comment
+
+*/
+// hey stranger
+//wyd
+
+
+
+/*to add use +
+to subtract use -
+to multiply *
+to divide /
+*/
+
+// to print to the console type console.log()
+
+//to do an operation do the the following
+
+console.log(1+1);
+
+//try multipling 2 times 2 together and printing it to the console!
+
+console.log(2*2);
+
+
+//great now print out the rest of your times tables!
+
+console.log(2*4);
+console.log(4*4);
+console.log(6*6);
+console.log(7*8);
+console.log(8*9);
+console.log(3*3);
+console.log(9*9);
+console.log(12*11);
+console.log(9*4);
+console.log(6*9);
+console.log('2*3='+2*3);
+console.log(2*4);
+console.log(2*5);
+
+
+//Now it's time to divide. 64 is a highly divisable number.
+//try dividing it by multiples of 2 (numbers like 4 aka 2x2=4)
+
+
+
+
+/* awesome now you know about numbers and console logs but did
+you know there's other data types???
+*/
+
+/*
+String: Represents text and is enclosed in single, double, or backticks
+(template literals).
+Example:
+ */
+let name = "John";
+let message = `Hello, ${name}!`;
+//or
+let message2 = 'hello' + name;
+
+/*
+Boolean: Represents a binary value, true or false, often used for 
+conditional statements.
+0, null, and undefined are falsey values.
+Example: 
+*/
+let isOnline = true;
+let isWorking = false;
+
+
+/*
+Array: A collection of values, indexed by position, and can contain mixed data types.
+Example:
+*/
+let colors = ["red", "green", "blue"];
+let mixedArray = [1, "two", true];
+
+/*
+Object: Represents a collection of key-value pairs, allowing you to store and access data with named properties.
+Example:
+*/
+let person = {
+    name: "Alice",
+    age: 25,
+    isStudent: true
+};
+
+
+/*
+Function: A block of reusable code that can be called with arguments and can return a value.
+you can call a function by function()
+Example:
+ */
+function add(a, b) {
+    return a + b;
+}
+
+
+/*
+Undefined: Represents a variable that has been declared but not assigned a value.
+Example:
+ */
+let x;
+console.log(x); // Outputs: undefined
+
+
+/*
+Null: Represents the intentional absence of any object value or no value at all.
+Example:
+ */
+let empty = null;
+
+
+
+/*
+Symbol: A unique and immutable data type often used as object property keys.
+Example: 
+*/
+const uniqueKey = Symbol('description');
+
+
+
